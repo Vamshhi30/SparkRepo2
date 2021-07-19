b427daa4ab2a9cefa54e3d8c81118f4610c0297f
Vamshhi <Vamshhi@30.159.213.125>
2021-07-19 17:33:20 +0530

    git push test

---
+++ Sample_Test/src/main/scala/com/vamshhi/spark/SparkObj.scala
@@ -0,0 +1,10 @@
+package com.vamshhi.spark
+
+object SparkObj {
+  
+  def main(args:Array[String]):Unit = {
+    
+    print("Hello welcome to apache spark development with python and scala")
+  }
+  
+}
