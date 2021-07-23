f676112f9555adc19ce7b916483ed5ee82fd3b92
Vamshhi <Vamshhi@30.159.214.236>
2021-07-23 23:12:46 +0530

    Spark project 2 updated

--- Spark_Project2_RDS_SparkHBase/src/main/scala/com/vamshhi/spark/Spark_Project2.scala
+++ Spark_Project2_RDS_SparkHBase/src/main/scala/com/vamshhi/spark/Spark_Project2.scala
@@ -9,36 +9,36 @@ object Spark_Project2
 {
 	def main(args:Array[String]):Unit = 
 		{
-
 				val spark = SparkSession.builder().appName("Spark Project2").master("local[*]").getOrCreate()
-						val sc = spark.sparkContext
-						sc.setLogLevel("Error")
-						val schema_df = spark.read.format("csv").option("header","true").load("file:///C:/Data/Spark_project3_source/schema.txt")
-						schema_df.show() 
+				val sc = spark.sparkContext
+				sc.setLogLevel("Error")
+				val schema_df = spark.read.format("csv").option("header","true").load("file:///C:/Data/Spark_project3_source/schema.txt")
+				schema_df.show() 
 
-						val schema_list = schema_df.select("*").collect().toList
-						//print(schema_list)
+				val schema_list = schema_df.select("*").collect().toList
+				//print(schema_list)
 
-						val str1 = 
-						s""" 
-						{
-						"table":{"namespace":"default","name":"hbase_tract11"},
-						"rowkey":"rowkey",
-						"columns":
-						{
-						"master_id" : { "cf" : "rowkey", "col" : "rowkey","type" : "string"},
-						"""+"\n"
-
-						val str2 = StringBuilder.newBuilder
-						for(schema <- schema_list) 
-						{
-							val scolumn = schema.getString(1)
-									val hcolumn = schema.getString(0)
-									val str = s""" "$scolumn" : { "cf" : "cf" ,"col" : "$hcolumn","type" : "string"},""" + "\n"
-											str2++= str
-						}
-				    val str3 = "}"
-						val catalog = str1+str2.toString.replaceAll(",$", "")+str3
-						println(catalog)
+				val str1 = 
+				s""" 
+				|{
+				|"table" : { "namespace" : "default", "name" : "hbase_tract11"},
+				|"rowkey" : "rowkey",
+				|"columns" :
+				|{
+				|"master_id" : { "cf" : "rowkey", "col" : "rowkey","type" : "string"},
+				""".stripMargin+"\n"
+						
+				//dynamic HBase catalog string creation
+				val str2 = StringBuilder.newBuilder
+				for(schema <- schema_list) 
+				{
+					val scolumn = schema.getString(1)
+					val hcolumn = schema.getString(0)
+					val str = s""" "$scolumn" : { "cf" : "cf" ,"col" : "$hcolumn","type" : "string"},""" + "\n"
+					str2++= str
+				}
+				  val str3 = "}"
+					val catalog = str1+str2.toString.replaceAll(",$", "")+str3
+					println(catalog)
 		} 
 }
